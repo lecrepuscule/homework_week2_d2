db5b2809139ebdb5fa277a0d351029aedab33e9f
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-18 19:52:38 +0100

    before changing syntax

--- mta/js/mta.js
+++ mta/js/mta.js
@@ -1,3 +1,5 @@
+var SubwayLine = SubwayLine || {};
+
 function SubwayLine (line, stations, intersections){
   this.line= line;
   this.stations= stations;
@@ -5,9 +7,9 @@ function SubwayLine (line, stations, intersections){
 }
 
 SubwayLine.prototype.getDistance = function(station1, station2){
-  var location1 = this.stations.indexOf(station1);
+  var location1 = this.locateStation(station1);
   var location2 = this.stations.indexOf(station2);
-  if (location1 !== -1 && location2 !== -1) {
+  if (this.locateStation(station1) && location2 !== -1) {
     return Math.abs(location1 - location2);
   }
   else {
@@ -16,8 +18,12 @@ SubwayLine.prototype.getDistance = function(station1, station2){
   }
 }
 
-// SubwayLine.prototype.locateStation = function(station) {
-//   return this.stations.indexOf(station) === -1 ? null : this.stations.indexOf(station);
+SubwayLine.prototype.locateStation = function(station) {
+  return this.stations.indexOf(station) === -1 ? null : this.stations.indexOf(station);
+}
+
+// SubwayLine.prototype.hasStation = function(station){
+//   return this.stations.indexOf(station) === -1 ? false : true;
 // }
 
 var line6 = new SubwayLine("6", ["GrandCentral", "33rd", "28th", "23rd", "UnionSquare", "AstorPlace"], ["UnionSquare"]);
@@ -27,13 +33,15 @@ var lineN = new SubwayLine("N", ["TimeSquare", "34th", "28th", "23rd", "UnionSqu
 var lineL = new SubwayLine("L", ["8th", "6th", "UnionSquare", "3rd", "1st"], ["UnionSquare"]);
 
 var test = line6.getDistance("33rd", "UnionSquare");
+// var test2 = lineL.getDistanceToIntersections("8th", "UnionSquare");
 
 console.log(test);
+console.log(test2);
 
-// can be expanded when there are multiple intersections on each line
-// Subwayline.prototype.getDistanceToIntersections = function(station, intersection) {
+//can be expanded when there are multiple intersections on each line
+
+// SubwayLine.prototype.getDistanceToIntersections = function(station, intersection) {
 //   if (intersection) {
-
+//     return this.getDistance(station1, intersection);
 //   }
-//   this.intersections.
 // }
